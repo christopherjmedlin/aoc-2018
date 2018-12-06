b2b06d4dc77c8b8a7245c1621f2330d589c2f29b
Christopher Medlin <christopherjmedlin@gmail.com>
2018-12-06 10:48:00 -0800

    Day 5 Part 1

--- src/dayfive.rs
+++ src/dayfive.rs
@@ -10,6 +10,7 @@ fn part_one(input: &Vec<String>) -> String {
     while i < char_vector.len() - 1 {
         let character = char_vector[i];
         let ascii_code = character as u8;
+        let mut reaction_found = false;
         
         if ascii_code + 32 == char_vector[i + 1] as u8 ||
            ascii_code - 32 == char_vector[i + 1] as u8 {
@@ -18,14 +19,41 @@ fn part_one(input: &Vec<String>) -> String {
             if i >= 2 {
                 i -= 2;
             }
-            println!("{}{}", char_vector[i], char_vector[i + 1]);
+            reaction_found = true;
         }
-        i += 1;
+        if !reaction_found {
+            i += 1;
+        }
     }
 
     char_vector.len().to_string()
 }
 
 fn part_two(input: &Vec<String>) -> String {
-    String::new()
+    let char_vector: Vec<char> = input[0].chars().collect();
+    
+    let mut smallest_length: usize = 50000;
+    for ascii in 65..=90 {
+        let mut new_char_vector = char_vector.clone();
+        let mut i: usize = 0;
+        
+        let mut i = 0;
+        while i < new_char_vector.len() - 1 {
+            let ascii_code = new_char_vector[i] as u8;
+
+            if ascii_code == ascii || ascii_code == ascii + 32 {
+                println!("{}", new_char_vector[i]);
+                new_char_vector.remove(i);
+            }
+            
+            i += 1;
+        }
+        
+        let length = new_char_vector.len();
+        if length < smallest_length {
+            smallest_length = length;
+        }
+    }
+
+    smallest_length.to_string()
 }
